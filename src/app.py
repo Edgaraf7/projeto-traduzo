9bf80c9580bc2cc074c094af3ababe150c30bf65
edgar <edgar.dev22@gmail.com>
2024-05-13 15:22:09 -0300

    chore: add controller 1/3 & 2/3

--- src/app.py
+++ src/app.py
@@ -1,16 +1,81 @@
-from flask import Flask
-
+from flask import Flask, request, render_template
 from controllers.admin_controller import admin_controller
+from src.models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
 
 from os import environ
 from waitress import serve
-
 
 app = Flask(__name__)
 app.template_folder = "views/templates"
 app.static_folder = "views/static"
 
 app.register_blueprint(admin_controller, url_prefix="/admin")
+
+
+@app.route("/", methods=['GET'])
+def home():
+    languages = LanguageModel.list_dicts()
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate="O que deseja traduzir?",
+        translate_from="pt",
+        translate_to="en",
+        translated="What do you want to translate?"
+    )
+
+
+@app.route("/", methods=['POST'])
+def translate_text():
+    # Verifica se o parâmetro 'reverse' está presente na solicitação POST
+    if 'reverse' in request.form:
+        # Se estiver presente, trata-se de uma solicitação de reversão
+        return reverse_translation()
+
+    # Caso contrário, trata-se de uma solicitação normal de tradução
+    text_to_translate = request.form['text-to-translate']
+    translate_from = request.form['translate-from']
+    translate_to = request.form['translate-to']
+
+    translator = GoogleTranslator(source=translate_from, target=translate_to)
+    translated_text = translator.translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated_text
+    )
+
+
+@app.route("/reverse", methods=['POST'])
+def reverse_translation():
+    text_to_translate = request.form['text-to-translate']
+    translate_from = request.form['translate-from']
+    translate_to = request.form['translate-to']
+
+    # Invertendo os idiomas
+    translator = GoogleTranslator(source=translate_to, target=translate_from)
+    translated_text = translator.translate(text_to_translate)
+
+    # Invertendo os textos original e traduzido
+    text_to_translate, translated_text = translated_text, text_to_translate
+
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=text_to_translate,
+        translate_from=translate_to,  # Agora é o idioma de origem invertido
+        translate_to=translate_from,  # Agora é o idioma de destino invertido
+        translated=translated_text
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 def start_server(host="0.0.0.0", port=8000):
